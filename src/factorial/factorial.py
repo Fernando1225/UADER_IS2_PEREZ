68c33180a1c330c16f11c87492bda638e2a0075a
Fernando Perez <ffernandoj.perez@gmail.com>
2023-03-27 11:36:19 -0300

    Modificando -desde -hasta a un parametro y 60

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -7,47 +7,43 @@
 #*-------------------------------------------------------------------------*
 import sys
 
-def factorial(num, num2): 
+def factorial(num): 
     factoriales = []
-    if (num < 0 or num2 < 0): 
+    if num < 0: 
         print("Factorial de un número negativo no existe")
 
-    elif num2 == 0: 
+    elif num == 0: 
         return 1
         
     else:
         num3 = num
-        while(num3<=num2):
-            num = num3
-            fact = 1
-            while(num > 1): 
-                fact *= num 
-                num -= 1
-            num3 += 1
-            factoriales.append(fact)
-        return factoriales 
+        fact = 1
+        while(num > 1): 
+            fact *= num 
+            num -= 1
 
+        factoriales.append(fact)
+
+        fact = 60
+        num = num3
+        while (num <= 60):
+            fact *= num
+            num +=1
+        factoriales.append(fact)
+
+        return factoriales
 
 if len(sys.argv) == 0:
     print("Debe informar un número!")
     sys.exit()
 
 if (len(sys.argv) == 1):
-    numeros = input('Ingrese numeros extremos! (ej: 4-8) ')
-    numeros = numeros.split('-')
-    num=int(numeros[0])
-    num2 = int(numeros[1])
-
+    num = int(input('Ingrese un numero! '))
 else:
+    num=int(sys.argv[1])
 
-    systemvar = sys.argv[1]
-    systemvar = systemvar.split('-')
-
-    num=int(systemvar[0])
-    num2 = int(systemvar[1])
-
-
-print("Factoriales desde", num, " hasta", num2, " son:", factorial(num, num2))
+print("Factorial -hasta ",num,"! es ", factorial(num)[0]) 
+print("Factorial desde- ",num,"! hasta 60 es ", factorial(num)[1])
 
 # print("Factorial ",num,"! es ", factorial(num)) 
 
