c7f57c2377c8d57dac361bdcff928000b26da155
Fernando Perez <ffernandoj.perez@gmail.com>
2023-04-20 20:35:38 -0300

    Agregando documentacion y ejercicios

--- src/patrones creacionales/TP3_3_A.py
+++ src/patrones creacionales/TP3_3_A.py
@@ -2,31 +2,32 @@ from abc import ABC, abstractmethod
 
 class Hamburguesa(ABC):
     @abstractmethod
-    def entrega(self):
+
+    def tipo_entrega(self):
         pass
 
 class HamburguesaMostrador(Hamburguesa):
-    def entrega(self):
+    def tipo_entrega(self):
         print("La hamburguesa está lista para ser retirada en el mostrador.")
 
 class HamburguesaRetirada(Hamburguesa):
-    def entrega(self):
+    def tipo_entrega(self):
         print("La hamburguesa está lista para ser retirada por el cliente.")
 
 class HamburguesaDelivery(Hamburguesa):
-    def entrega(self):
+    def tipo_entrega(self):
         print("La hamburguesa está lista para ser enviada por delivery.")
 
 class HamburguesaFactory:
-    def __init__(self, entrega):
-        self.entrega = entrega
+    def __init__(self, tipo_entrega):
+        self.tipo_entrega = tipo_entrega
     
     def crear_hamburguesa(self):
-        if self.entrega == "mostrador":
+        if self.tipo_entrega == "mostrador":
             return HamburguesaMostrador()
-        elif self.entrega == "retirada":
+        elif self.tipo_entrega == "retirada":
             return HamburguesaRetirada()
-        elif self.entrega == "delivery":
+        elif self.tipo_entrega == "delivery":
             return HamburguesaDelivery()
         else:
             raise ValueError("Tipo de entrega no válido.")
@@ -37,11 +38,11 @@ print("Hamburguesa 1:")
 factory = HamburguesaFactory("mostrador")
 hamburguesa = factory.crear_hamburguesa()
 # Print el tipo de entrega de la hamburguesa
-hamburguesa.entrega()  
+hamburguesa.tipo_entrega()  
 
 print()
 print("Hamburguesa 2:")
 factory = HamburguesaFactory("delivery")
 hamburguesa = factory.crear_hamburguesa()
 # Print el tipo de entrega de la hamburguesa
-hamburguesa.entrega()  
+hamburguesa.tipo_entrega()  
